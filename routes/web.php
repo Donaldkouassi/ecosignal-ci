4cf59d75f84305ebb8d51802ff59e37a3119d4a1
Donaldkouassi <kouassy01@gmail.com>
2026-07-17 15:20:56 +0000

    Corriger la route d'accueil de l'API

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'application' => 'EcoSignal CI API',
+        'status' => 'online',
+        'message' => 'Le backend fonctionne correctement.',
+    ]);
 });
